911a013d6413f4625744c967a46d9899b28b0911
Massimo Maggi <massimo@mmmm.it>
2012-05-12 19:30:40 +0200

    Initial BBque support - singlethreaded

---
+++ src/exec/bbque.cpp
@@ -0,0 +1,45 @@
+#include "bbque.hpp"
+#include "main.hpp"
+
+void ExecMethod::bbque::RunAndWait(unsigned long numberoftimes)
+{
+  RTLIB_Services_t *rtlib;
+  RTLIB_Init("DPAcalc", &rtlib);
+  assert(rtlib);
+  shared_ptr<ExecMethod::BbqueClass> bc = shared_ptr<ExecMethod::BbqueClass>(new ExecMethod::BbqueClass("DPAcalc","DPAcalcRecipe",rtlib,numberoftimes));
+  assert(bc->isRegistered());
+  bc->Start();
+  bc->WaitCompletion();
+  //la distruzione avviene con la fine di questo metodo (grazie inventore dello shared_ptr!).
+}
+
+ExecMethod::BbqueClass::BbqueClass(const string& name, const string& recipe, RTLIB_Services_t* rtlib,unsigned long _tot):BbqueEXC(name, recipe, rtlib),mtx()
+{
+  tot=_tot;
+  num=0;
+cout << "BbqueClass initialized."<<endl;
+}
+RTLIB_ExitCode_t ExecMethod::BbqueClass::onConfigure(uint8_t awm_id)
+{
+return RTLIB_OK;
+}
+RTLIB_ExitCode_t ExecMethod::BbqueClass::onMonitor()
+{
+return RTLIB_OK;
+}
+RTLIB_ExitCode_t ExecMethod::BbqueClass::onRun()
+{
+  mtx.lock();
+  num++;
+  if(num > tot){
+    mtx.unlock();
+    return RTLIB_EXC_WORKLOAD_NONE;
+  }
+  mtx.unlock();
+ DPA::instance()->doRun();
+ return RTLIB_OK;
+}
+RTLIB_ExitCode_t ExecMethod::BbqueClass::onSetup()
+{
+return RTLIB_OK;
+}
---
+++ src/exec/bbque.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "dpacalc.h"
+#include "base.hpp"
+#include <mutex>
+#include <bbque/bbque_exc.h>
+
+using bbque::rtlib::BbqueEXC;
+
+namespace ExecMethod {
+class bbque: public base {
+public:
+    bbque(TCLAP::CmdLine& cmd):base(cmd) {}
+    virtual void RunAndWait(unsigned long numberoftimes);  //What should I run? DPA::onRun(); called on the right thread.
+};
+class BbqueClass: public BbqueEXC{
+public:	
+      BbqueClass(std::string const & name,
+			std::string const & recipe,
+			RTLIB_Services_t *rtlib,unsigned long _tot);
+protected:
+	unsigned long tot;
+	unsigned long num;
+	std::mutex mtx;
+  	RTLIB_ExitCode_t onSetup();
+	RTLIB_ExitCode_t onConfigure(uint8_t awm_id);
+	RTLIB_ExitCode_t onRun();
+	RTLIB_ExitCode_t onMonitor();
+  
+};
+}
+
