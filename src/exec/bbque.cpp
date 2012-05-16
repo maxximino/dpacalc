386715141245d0dbe1aba24db890a0bd42a11754
Massimo Maggi <massimo@mmmm.it>
2012-05-16 22:01:03 +0200

    Aggiunto supporto per Barbeque - https://bitbucket.org/bosp/barbeque

---
+++ src/exec/bbque.cpp
@@ -0,0 +1,71 @@
+#include "bbque.hpp"
+#include "main.hpp"
+void prefetchthread();
+void threadfunction();
+
+void ExecMethod::bbque::RunAndWait(unsigned long numberoftimes)
+{
+  std::thread prefetcht = thread(prefetchthread);
+  RTLIB_Services_t *rtlib;
+  RTLIB_Init("DPAcalc", &rtlib);
+  assert(rtlib);
+  shared_ptr<ExecMethod::BbqueClass> bc = shared_ptr<ExecMethod::BbqueClass>(new ExecMethod::BbqueClass("DPAcalc","DPAcalcRecipe",rtlib,numberoftimes));
+  assert(bc->isRegistered());
+  bc->Start();
+  bc->WaitCompletion();
+  prefetcht.join();
+  //la distruzione avviene con la fine di questo metodo (grazie inventore dello shared_ptr!).
+}
+
+ExecMethod::BbqueClass::BbqueClass(const string& name, const string& recipe, RTLIB_Services_t* rtlib,unsigned long _tot):BbqueEXC(name, recipe, rtlib),mtx()
+{
+  tot=_tot;
+  num=0;
+cout << "BbqueClass initialized."<<endl;
+}
+RTLIB_ExitCode_t ExecMethod::BbqueClass::onConfigure(uint8_t awm_id)
+{
+  numthreads=awm_id+1;
+return RTLIB_OK;
+}
+RTLIB_ExitCode_t ExecMethod::BbqueClass::onMonitor()
+{
+return RTLIB_OK;
+}
+RTLIB_ExitCode_t ExecMethod::BbqueClass::onRun()
+{
+ unsigned int oldnum;
+  mtx.lock();
+  
+  if(num >= tot){
+    mtx.unlock();
+    return RTLIB_EXC_WORKLOAD_NONE;
+  }
+  oldnum=num;
+    vector<thread> thrs;
+    for(; num<oldnum+numthreads; num++) {
+	if(num >= tot){
+	  break;
+	}
+        thrs.push_back(thread(threadfunction));
+    }
+    mtx.unlock();
+    for (auto t = thrs.begin(); t != thrs.end(); ++t) {
+        t->join();
+    }
+
+ return RTLIB_OK;
+}
+RTLIB_ExitCode_t ExecMethod::BbqueClass::onSetup()
+{
+return RTLIB_OK;
+}
+
+void prefetchthread() {
+    DPA::instance()->prefetch();
+}
+
+void threadfunction()
+{
+        DPA::instance()->doRun();
+}
